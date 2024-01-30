fa7de32c8d91a6cf0a37c207c013c0c3f5541ac2
Jiwoon <kfromh0136@Naver.com>
2024-01-31 02:36:28 +0900

    [feat][client] react router setting
    - sass, mui-icons 라이브러리 설치
    - App.tsx Router 설정
    
    **bug**
    eslint 에러로 설정파일 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import './App.css';
+import 'assets/style/App.scss';
+import { Route, Routes } from 'react-router-dom';
+import ExtensionManagement from 'pages/ExtensionManagement';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<ExtensionManagement />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
