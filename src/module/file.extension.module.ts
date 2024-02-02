86f32a239c7ada1229efa019ec152f410302c3db
Jiwoon <kfromh0136@Naver.com>
2024-02-02 16:04:49 +0900

    [hofix] missing module setting

--- src/module/file.extension.module.ts
+++ src/module/file.extension.module.ts
@@ -4,10 +4,11 @@ import { FileController } from 'src/controller/file.controller';
 import { FileExtensionController } from 'src/controller/file.extension.controller';
 import { FileExtension } from 'src/model/dao/file.extension.entity';
 import { FileExtensionService } from 'src/service/file.extension.service';
+import { FileService } from 'src/service/file.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FileExtension])],
   controllers: [FileExtensionController, FileController],
-  providers: [FileExtensionService],
+  providers: [FileExtensionService, FileService],
 })
 export class FileExtensionModule {}
